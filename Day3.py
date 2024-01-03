076778abd62c01391ab4bb202ff61ca79f9b7c10
Radu Simu <Simu.Oc.Radu@student.utcluj.ro>
2024-01-03 03:45:34 +0200

    Day3 completed

--- Day3.py
+++ Day3.py
@@ -1,38 +1,48 @@
-def has_non_dot_or_number_neighbor(matrix, row, col):
+def check_whole_number(i,j, matrix, freq):
+    j_initial = j
+    number = 0
+    p=1
+    while j>=0 and matrix[i][j].isnumeric():
+        number = p*int(matrix[i][j])+number
+        freq[i][j] = 1
+        p*=10
+        j-=1
+    j_initial+=1
+    while j_initial<len(matrix[0]) and matrix[i][j_initial].isnumeric():
+        freq[i][j_initial] = 1
+        number = number*10+int(matrix[i][j_initial])
+        j_initial+=1
+    return number
+
+def has_number(matrix, row, col):
+    freq = [[0 for i in range(len(matrix[0]))] for j in range(len(matrix))]
+    nr_numbers = 0
     rows = len(matrix)
     cols = len(matrix[0])
-
-    directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
-
+    sum=1
+    directions = [(0, -1), (0, 1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
     for dr, dc in directions:
         new_row, new_col = row + dr, col + dc
-
         if 0 <= new_row < rows and 0 <= new_col < cols:
-            if matrix[new_row][new_col] not in ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-                return True
+            if matrix[new_row][new_col].isnumeric() and freq[new_row][new_col]==0:
+                number = check_whole_number(new_row, new_col, matrix, freq)
+                if number>0:
+                    nr_numbers+=1
+                    sum *= number
+    if nr_numbers == 2:
+        return sum
+    return 0
 
-    return False
-
-def part_numbers(file):
+def part_numbers2(file):
     with open(file) as f:
-        numbers = []
+        suma=0
         lines = f.readlines()
         listc = [list(string)[:-1] for string in lines]
-        for line_number in range(len(lines)):
-            ok = 0
-            nr = 0
-            for letter in range(len(lines[line_number])):
-                if lines[line_number][letter].isnumeric():
-                    nr = nr * 10 + int(lines[line_number][letter])
-                    if has_non_dot_or_number_neighbor(listc, line_number, letter):
-                        ok = 1
-                else:
-                    if nr != 0 and ok == 1:
-                        numbers.append(nr)
-                    ok = 0
-                    nr = 0
-    return numbers
-                    
+        for line_number in range(len(listc)):
+            for char_number in range(len(listc[0])):
+                if listc[line_number][char_number]=='*':
+                    suma += has_number(listc, line_number, char_number)
 
-num = part_numbers('./input.txt')
-print(sum(num))
+    return suma
+
+print(part_numbers2('./input.txt'))
