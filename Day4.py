66b7d454980669fd2387ef0d0e83e0622017cfad
Radu Simu <Simu.Oc.Radu@student.utcluj.ro>
2024-01-04 02:20:14 +0200

    Day4 part2

--- Day4.py
+++ Day4.py
@@ -1,6 +1,5 @@
 from math import floor
 
-
 def input_read(filename):
     winning_numbers = []
     personal_numbers = []
@@ -13,12 +12,27 @@ def input_read(filename):
             personal_numbers.append(numbers.split())
     return winning_numbers, personal_numbers
 
-w_nr, tries = input_read('input.txt')
-suma = 0
-for i in range(len(tries)):
-    p=0.5
-    for j in tries[i]:
-        if j in w_nr[i]:
-            p*=2
-    suma+=floor(int(p))
-print(suma)
+def part1():
+    w_nr, tries = input_read('input.txt')
+    suma = 0
+    for i in range(len(tries)):
+        p=0.5
+        for j in tries[i]:
+            if j in w_nr[i]:
+                p*=2
+        suma+=floor(int(p))
+    print(suma)
+
+def part2():
+    w_nr, tries = input_read('input.txt')
+    card_number = [1 for i in range(len(w_nr))]
+    for i in range(len(tries)):
+        nr_copies = 0
+        for j in tries[i]:
+            if j in w_nr[i]:
+                nr_copies += 1
+                if nr_copies+i < len(tries):
+                    card_number[nr_copies+i] += card_number[i]
+    print(sum(card_number))
+
+part2()
